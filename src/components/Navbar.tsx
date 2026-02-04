069f9a20303f6935d4d745125109238bd6b9d679
Erhahon Imafidon <mafiosco4@gmail.com>
2026-02-04 15:20:03 +0100

    feat: enhance Navbar component with styling and logo constant

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 import { NavLink, Link } from 'react-router';
 
+const Logo = '/logo.png';
+
 const Navbar = () => {
-    return <header>Navbar</header>;
+    return (
+        <header className="container font-lato fixed inset-x-0 top-0">
+            Navbar
+        </header>
+    );
 };
 
 export default Navbar;
